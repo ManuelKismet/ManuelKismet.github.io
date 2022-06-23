161028716fef17f53a4823615a050bbb2f1750a9
ManuelKismet <kismetkoranteng@yahoo.com>
2022-06-23 01:49:29 +0000

    none

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function submitemail(){
+	alert("hello")
     set fso = CreateObject("Scripting.FileSystemObject");  
     set s = fso.CreateTextFile(".test.txt", True);
     s.writeline("HI");
     s.writeline("Bye");
     s.writeline("-----------------------------");
     s.Close();
+    
+    alert("done")
     }
