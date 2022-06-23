d59e44656abd1dd5eff564177073e931ffff3457
ManuelKismet <kismetkoranteng@yahoo.com>
2022-06-23 01:07:00 +0000

    added script

---
+++ script.js
@@ -0,0 +1,6 @@
+function submitemial(){
+  var txt = new ActiveXObject("Scripting.FileSystemObject");
+                var s = txt.CreateTextFile("11.txt", true);
+                s.WriteLine('Hello');
+                s.Close();
+}
